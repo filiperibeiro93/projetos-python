bb82791cac64996e4c8c9788468372e4eed3d55b
Filipe Ribeiro <facribeiro.93@gmail.com>
2022-06-07 12:57:00 -0300

    updating post method using database

--- api-flask/resources/Hotel.py
+++ api-flask/resources/Hotel.py
@@ -1,3 +1,4 @@
+from email import message
 from flask_restful import Resource, reqparse
 from models.hotel_modelo import HotelModel
 
@@ -36,25 +37,19 @@ class Hotel(Resource):
   argumentos.add_argument('diaria')
   argumentos.add_argument('cidade')
 
-  def find_hotel(hotel_id):
-    for hotel in hoteis:
-      if hotel['hotel_id'] == hotel_id:
-        return hotel
-    return None
-
   def get(self, hotel_id):
-    hotel = Hotel.find_hotel(hotel_id)
+    hotel = HotelModel.find_hotel(hotel_id)
     if hotel:
       return hotel
     return {'message': 'Hotel not found.'}, 404 # not found
 
   def post(self, hotel_id):
+    if HotelModel.find_hotel(hotel_id):
+      return {"message": "Hotel id '{}' already exists.".format(hotel_id)}, 400 # bad request
     dados = Hotel.argumentos.parse_args()
-    hotel_objeto = HotelModel(hotel_id, **dados)
-    novo_hotel = hotel_objeto.json()
-    # novo_hotel = { 'hotel_id': hotel_id, **dados }
-    hoteis.append(novo_hotel)
-    return novo_hotel, 200
+    hotel = HotelModel(hotel_id, **dados)
+    hotel.save_hotel()
+    return hotel.json()
 
   def put(self, hotel_id):
     dados = Hotel.argumentos.parse_args()
