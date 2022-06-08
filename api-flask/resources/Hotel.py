7e15da3c1196810a31eee759640d539a8c8a8b44
Filipe Ribeiro <facribeiro.93@gmail.com>
2022-06-08 00:22:46 -0300

    adding login and logout

--- api-flask/resources/Hotel.py
+++ api-flask/resources/Hotel.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.hotel_modelo import HotelModel
+from flask_jwt_extended import jwt_required
 
 
 class Hoteis(Resource):
@@ -23,6 +24,7 @@ class Hotel(Resource):
             return hotel.json()
         return {'message': 'Hotel not found.'}, 404  # not found
 
+    @jwt_required()
     def post(self, hotel_id):
         if HotelModel.find_hotel(hotel_id):
             # bad request
@@ -36,6 +38,7 @@ class Hotel(Resource):
             return {'message': 'An internal error has occurred trying to save hotel.'}, 500
         return hotel.json()
 
+    @jwt_required()
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
 
@@ -56,6 +59,7 @@ class Hotel(Resource):
             return {'message': 'An internal error has occurred trying to save hotel.'}, 500
         return hotel.json(), 201  # created
 
+    @jwt_required()
     def delete(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
--- api-flask/resources/Usuario.py
+++ api-flask/resources/Usuario.py
@@ -1,5 +1,13 @@
 from flask_restful import Resource, reqparse
 from models.usuario_modelo import UserModel
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt
+from werkzeug.security import safe_str_cmp
+from blacklist import BLACKLIST
+
+
+atributos = reqparse.RequestParser()
+atributos.add_argument('login', type=str, required=True, help="The field 'login' must be informed")
+atributos.add_argument('senha', type=str, required=True, help="The field 'senha' must be informed")
 
 
 class Users(Resource):
@@ -9,9 +17,6 @@ class Users(Resource):
 
 
 class User(Resource):
-    argumentos = reqparse.RequestParser()
-    argumentos.add_argument('login', type=str, required=True, help="The field 'login' must be informed")
-    argumentos.add_argument('senha', type=str, required=True, help="The field 'senha' must be informed")
 
     # /usuarios/{user_id}
     def get(self, user_id):
@@ -20,6 +25,7 @@ class User(Resource):
             return user.json()
         return {'message': 'User not found.'}, 404  # not found
 
+    @jwt_required()
     def delete(self, user_id):
         user = UserModel.find_user(user_id)
         if user:
@@ -30,17 +36,38 @@ class User(Resource):
                 return {'message': 'An internal error has occurred trying to delete user.'}, 500
         return {'message': 'User not found.'}, 404
 
-class UserRegister(Resource):
-  # /cadastro
-  def post(self):
-    atributos = reqparse.RequestParser()
-    atributos.add_argument('login', type=str, required=True, help="The field 'login' must be informed")
-    atributos.add_argument('senha', type=str, required=True, help="The field 'senha' must be informed")
-    dados = atributos.parse_args()
 
-    if UserModel.find_by_login(dados['login']):
-      return {"message": "The login '{}' already exists.".format(dados['login'])}
-    
-    user = UserModel(**dados)
-    user.save_user()
-    return {"message": "User created!"}, 201 # created
+class UserRegister(Resource):
+    # /cadastro
+    def post(self):
+        dados = atributos.parse_args()
+
+        if UserModel.find_by_login(dados['login']):
+            return {"message": "The login '{}' already exists.".format(dados['login'])}
+
+        user = UserModel(**dados)
+        user.save_user()
+        return {"message": "User created!"}, 201  # created
+
+
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        dados = atributos.parse_args()
+
+        user = UserModel.find_by_login(dados['login'])
+
+        if user and safe_str_cmp(user.senha, dados['senha']):
+            token_de_acesso = create_access_token(identity=user.user_id)
+            return {'access_token': token_de_acesso}, 200
+        return {'message': 'Username or password incorrect'}, 401  # unauthorized
+
+class UserLogout(Resource):
+
+    @jwt_required()
+    def post(self):
+        jwt_id = get_jwt()['jti'] # JWT Token Identifier
+        BLACKLIST.add(jwt_id)
+        return {'message': 'Logged out!'}, 200
+
